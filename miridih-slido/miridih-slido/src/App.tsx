b01ddd997fa5309747f7475ea9c7b2d2d544e64e
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-07-22 01:27:04 +0900

    slido 리소스 및 mock json 파일 세팅

--- miridih-slido/miridih-slido/src/App.tsx
+++ miridih-slido/miridih-slido/src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface Feature {
+  id: number;
+  title: string;
+  content: string;
+  iconPath: string;
+  videoPath: string;
+}
 
 function App() {
-  return <div>미리디 slido 클론코딩 시작</div>;
+  const [featureList, setFeatureList] = useState<Feature[]>([]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      const response = await fetch("/mock/main-features.json");
+      const { result } = await response.json();
+      setFeatureList(result);
+    }
+
+    fetchItems();
+  }, []);
+
+  if (featureList.length > 0) {
+    return (
+      <>
+        <div>미리디 slido 클론코딩 시작</div>
+        <img src="https://blog.slido.com/wp-content/uploads/2020/04/blog-slido-QA-header-1200x800.jpg" />
+        <video src={featureList[0].videoPath} autoPlay loop playsInline muted />
+      </>
+    );
+  } else {
+    return <div>안뇽</div>;
+  }
 }
 
 export default App;
