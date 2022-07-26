751eb594c3ee45069805b5f4cd40d35096b0a1e0
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-07-26 23:50:52 +0900

    [slido] chore: emotion-reset lib설정 및 적용

--- miridih-slido/miridih-slido/src/App.tsx
+++ miridih-slido/miridih-slido/src/App.tsx
@@ -1,12 +1,33 @@
-import React, { useEffect, useState } from "react";
+/** @jsxImportSource @emotion/react */
+
+import { Global, css, jsx } from "@emotion/react";
+import resetLib from "emotion-reset";
 import PageLayout from "./layouts/PageLayout";
 import Home from "./pages/Home";
 
+const resetCss = css`
+  ${resetLib}
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+`;
+
 function App() {
   return (
-    <PageLayout>
-      <Home />
-    </PageLayout>
+    <>
+      <Global styles={resetCss} />
+      <PageLayout>
+        <Home />
+      </PageLayout>
+    </>
   );
 }
 
