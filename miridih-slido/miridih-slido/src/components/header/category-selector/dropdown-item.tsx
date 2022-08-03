603152c85ee4cd1781397ed5bdc026089cbabf81
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-08-03 22:17:40 +0900

    [slido] feat: 헤더 dropdown 좌측 영역 구성

--- miridih-slido/miridih-slido/src/components/header/category-selector/dropdown-item.tsx
+++ miridih-slido/miridih-slido/src/components/header/category-selector/dropdown-item.tsx
@@ -1,19 +1,38 @@
+/** @jsxImportSource @emotion/react */
+
+import { jsx, css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-const Container = styled.div`
+const Container = styled.div<{ selected: boolean }>`
   height: 48px;
+  padding: 0 24px;
 
-  &:hover {
-    background-color: #dddddd;
-  }
-`
+  white-space: nowrap;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${({ selected }) =>
+    selected &&
+    css`
+      background-color: #eeeeee;
+    `}
+`;
 
 interface Props {
   name: string;
+  selected: boolean;
+  onSelect: () => void;
 }
 
-const DropdownItem = ({ name }: Props) => {
-  return <Container>{name}</Container>
-}
+const DropdownItem = ({ name, selected, onSelect }: Props) => {
+  return (
+    <Container onMouseEnter={onSelect} selected={selected}>
+      {name}
+    </Container>
+  );
+};
 
 export default DropdownItem;
--- miridih-slido/miridih-slido/src/components/header/category-selector/sub-category-dropdown.tsx
+++ miridih-slido/miridih-slido/src/components/header/category-selector/sub-category-dropdown.tsx
@@ -2,29 +2,54 @@
 
 import { jsx } from "@emotion/react";
 import styled from "@emotion/styled";
+import { useState } from "react";
 import { SubCategory } from "../../../types/category";
 import DropdownItem from "./dropdown-item";
 
 const Container = styled.div`
   position: absolute;
-  width: 300px;
   left: 0;
-  top: calc(100% + 8px);
+  top: 100%;
+  padding: 24px 0;
 
   background-color: white;
   box-shadow: 0 4px 12px rgb(0 0 0 / 15%);
   border-radius: 4px;
   z-index: 99;
+
+  display: flex;
+`;
+
+const Left = styled.div`
+  border-right: 1px solid #eeeeee;
+`;
+
+const Right = styled.div`
+  width: 300px;
 `;
 
 interface Props {
-  list: SubCategory[]
+  list: SubCategory[];
 }
 
 const SubCategoryDropdown = ({ list }: Props) => {
-  return <Container>
-    {list.map(item => <DropdownItem key={item.id} name={item.name}/>)}
-  </Container>;
+  const [currentIdx, setCurrentIdx] = useState<number>(0);
+
+  return (
+    <Container>
+      <Left>
+        {list.map((item, idx) => (
+          <DropdownItem
+            key={item.id}
+            name={item.name}
+            selected={currentIdx === idx}
+            onSelect={setCurrentIdx.bind(this, idx)}
+          />
+        ))}
+      </Left>
+      <Right>{list[currentIdx].name}</Right>
+    </Container>
+  );
 };
 
 export default SubCategoryDropdown;
